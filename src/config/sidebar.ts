5bf1cb8656446f94033ff4a2e674397db85e3843
qiaoxr <qiaoxrcn@gmail.com>
2024-10-14 17:57:42 +0900

    add src/content/docs/en/sdk/guides/aws-deployment-not-use-an-alt-gas-token.mdx

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -390,6 +390,10 @@ export const getSidebar = () => {
             title: t("sidebar.sdk.digitalOcean"),
             url: formatUrl("sdk/guides/digital-ocean-alt-gas-token"),
           },
+          {
+            title: t("sidebar.sdk.awsDeploy"),
+            url: formatUrl("sdk/guides/aws-deployment-not-use-an-alt-gas-token"),
+          },
           // {
           //   title: t("sidebar.sdk.runOnArm64Mac"),
           //   url: formatUrl("sdk/guides/run-on-arm64-mac"),
